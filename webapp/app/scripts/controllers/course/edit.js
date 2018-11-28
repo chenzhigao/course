08f328a0407bb3697279949a7ba893e242c01706
chenzhigao <2934472815@qq.com>
2018-11-28 12:55:40 +0800

    初步修改

--- webapp/app/scripts/controllers/course/edit.js
+++ webapp/app/scripts/controllers/course/edit.js
@@ -8,10 +8,17 @@
  * Controller of the webappApp
  */
 angular.module('webappApp')
-  .controller('CourseEditCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('CourseEditCtrl', function ($stateParams, $http, $scope, $state) {
+    var self = this;
+    self.init = function(){
+    	var id = $stateParams.id;
+    	var url = 'http://127.0.0.1:8080/Course/' + id;
+    	$http.get(url)
+    	.then(function success(response){
+    		$scope.data = response.data;
+    	},function error(response){
+
+    	})
+
+    };
   });
--- webapp/app/scripts/controllers/login.js
+++ webapp/app/scripts/controllers/login.js
@@ -9,9 +9,28 @@
  */
 angular.module('webappApp')
   .controller('LoginCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    var self = this;
+
+    self.init = function(){
+    	$scope.user={
+    		username:'',
+    		password:''
+    	};
+    	// 显示出错信息
+    	$scope.showErrorBox = false;
+    };
+    self.submit = function(){
+    	teacher.login($scope.user)
+    	.then(function success(){
+    		$scope.showErrorBox = false;
+    		teacher.getCurrentLoginTeacher(function(){
+    			$location.url('/main');
+    		});
+    	},function error(){
+    		$scope.showErrorBox = true;
+    		console.log('error',response);
+    	});
+    };
+    self.init();
+    $scope.submit = self.submit;
   });
